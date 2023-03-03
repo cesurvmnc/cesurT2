77995d8c5e4e49997ad252ccc9420c8a52e9fcee
victor <victormanuelnavarrocamino@gmail.com>
2023-03-03 08:58:37 +0100

    primer commit

---
+++ index2.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sin título</title>
+</head>
+
+<body>
+
+<?php
+	
+	echo "Hola, Soy Victor M. Navarro GITHUB";
+
+?>
+</body>
+</html>
